dd13187c42e437956764c5acdb648c51c4e22194
Titouh-Hillal <hillaltitouh9@gmail.com>
2023-12-08 11:01:57 +0100

    Portfolio

--- index.js
+++ index.js
@@ -9,21 +9,6 @@
             }
             prevScrollpos = currentScrollPos;
         };
-        /*
-        const observer = new IntersectionObserver((entries) =>{
-            entries.forEach((entry)=>{
-                console.log(entry)
-                if(entry.isIntersecting){
-                    entry.target.classList.add('show');
-                }
-                else{
-                    entry.target.classList.remove('show');
-                }
-            });
-        });
-        const hiddenElements = document.querySelectorAll('.hidden');
-        hiddenElements.forEach((el) => observer.observe(el));*/
-        
 
         let section = document.querySelectorAll('section');
         let navLinks = document.querySelectorAll('a.nav-link');
